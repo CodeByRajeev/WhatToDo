ee94f9da41e3f80556e6baad5a12fec634623bf4
Rajeev Kommani <112249927+CodeByRajeev@users.noreply.github.com>
2023-09-10 18:09:54 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const questionForm = document.getElementById('questionForm');
+    const suggestionList = document.getElementById('suggestionList');
 
+    questionForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // Example user inputs (replace with actual data)
+        const mood = 'Energetic';
+        const activityCategory = 'Outdoor';
+        const locationMethod = 'gps';
+        const location = 'Central Park, New York'; // Example location
+        const groupSize = 'With friends';
+        const activityLevel = 'Highly active';
+        const duration = 'Medium';
+        const budget = 'Moderate';
+        const interests = ['Sports', 'Music']; // Example interests
+        const weather = 'Outdoor activities only';
+        const skillLevel = 'Intermediate';
+        const dietPreferences = ['Vegetarian']; // Example dietary preferences
+        const ageGroup = 'Young adults (20-35)';
+        const seasonalPreferences = ['Spring']; // Example seasonal preferences
+        const eventSpecifics = ['Music']; // Example event specifics
+
+        // Additional data collection based on your needs
+        const additionalData = {
+            // Collect additional user data here
+        };
+
+        // Logic to suggest activities based on user's inputs and additional data
+        const suggestions = getSuggestions(mood, activityCategory, location, groupSize, activityLevel, duration, budget, interests, weather, skillLevel, dietPreferences, ageGroup, seasonalPreferences, eventSpecifics, additionalData);
+
+        // Display suggestions
+        displaySuggestions(suggestions);
+    });
+
+    function getSuggestions(mood, activityCategory, location, groupSize, activityLevel, duration, budget, interests, weather, skillLevel, dietPreferences, ageGroup, seasonalPreferences, eventSpecifics, additionalData) {
+        // Implement your logic to suggest activities based on user's inputs and additional data
+        // Customize the suggestions based on the collected data
+        let suggestions = [];
+
+        // Generate suggestions based on user data and interests
+        // You can have a database of ideas and query it based on user preferences
+
+        // Example suggestions (replace with actual data from your database):
+        suggestions = [
+            { text: 'Play a game of soccer in the park', link: 'https://example.com/soccer' },
+            { text: 'Attend a live music concert', link: 'https://example.com/concert' },
+            { text: 'Go for a hike in the nearby trails', link: 'https://example.com/hike' },
+            { text: 'Join a local basketball tournament', link: 'https://example.com/basketball' },
+            { text: 'Visit a trendy new restaurant in town', link: 'https://example.com/restaurant' },
+            // Add more suggestions
+        ];
+
+        return suggestions;
+    }
+
+    function displaySuggestions(suggestions) {
+        // Display suggestions in the suggestionList element
+        suggestionList.innerHTML = '';
+        suggestions.forEach(activity => {
+            const li = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = activity.link;
+            link.textContent = activity.text;
+            li.appendChild(link);
+            suggestionList.appendChild(li);
+        });
+
+        // Show suggestions div
+        const suggestionsDiv = document.getElementById('suggestions');
+        suggestionsDiv.classList.remove('hidden');
+    }
+});
